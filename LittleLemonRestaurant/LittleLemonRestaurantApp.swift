235ccf4467c91df14db7075fb9ecb6e8a23e285b
Swain, Susrut (Cognizant) <Susrut.Swain@otiselevator.net>
2025-01-28 15:36:24 +0530

    Initial Commit

---
+++ LittleLemonRestaurant/LittleLemonRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonRestaurantApp.swift
+//  LittleLemonRestaurant
+//
+//  Created by Swain, Susrut (Cognizant) on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
