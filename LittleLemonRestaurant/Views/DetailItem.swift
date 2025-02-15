60cf696c07e55dd82021da5f861e82671f61459f
Swain, Susrut (Cognizant) <Susrut.Swain@otiselevator.net>
2025-02-15 13:20:49 +0530

    Add Detail Item view

--- LittleLemonRestaurant/Views/DetailItem.swift
+++ LittleLemonRestaurant/Views/DetailItem.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct DetailItem: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
+    let dish: Dish
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            AsyncImage(url: URL(string: dish.image ?? "")) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(minWidth: 150, minHeight: 150)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            
+            Text(dish.title ?? "")
+                .font(.subTitleFont())
+                .foregroundStyle(Color.primaryColor1)
+            Spacer(minLength: 20)
+            
+            Text(dish.descriptionDish ?? "")
+                .font(.regularText())
+                .foregroundStyle(Color.highlightColor2)
+                .multilineTextAlignment(.center)
+                .frame(width: .infinity, alignment: .center)
+            Spacer(minLength: 10)
+            
+            if #available(iOS 16.0, *) {
+                Text("$" + (String(dish.price)))
+                    .font(.highlightText())
+                    .foregroundColor(.primaryColor1)
+                    .monospaced()
+            } else {
+                Text("$" + (String(dish.price)))
+                    .font(.highlightText())
+                    .foregroundColor(.primaryColor1)
+            }
+            Spacer()
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .center)
     }
 }
 
 #Preview {
-    DetailItem()
+    DetailItem(dish: PersistenceController.oneDish())
 }
